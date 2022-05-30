7db3a8697266e4c13ed9b2d67f5955b74688be08
obelosto <olgaBelostotskaia@novi-education.nl>
2022-05-30 20:12:27 +0200

    Eerste upload

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Integer[] numeric = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        String[] alphabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "nul"};
+
+
+
+        Translator translator = new Translator( numeric, alphabetic );
+        boolean play = true;
+        String ongeldig = "ongeldige invoer";
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welkom bij de converter van getallen!");
+
+        while (play) {
+            System.out.println("Type 'x' om te stoppen \nType 'v' om te vertalen");
+            String input = scanner.nextLine();
+
+            if( input.equalsIgnoreCase("x") ){
+                play = false;
+            }
+            else if(input.equalsIgnoreCase("v")){
+                System.out.println("Type een cijfer in van 0 t/m 9");
+
+                int number = scanner.nextInt();
+                if(number < 10){
+                    String result = translator.translate(number);
+                    System.out.println("De vertaling van " + number + " is " + result);
+                    break;
+                }else{
+                    System.out.println(ongeldig);
+                }
+
+            }
+            else{
+                System.out.println(ongeldig);
+            }
+        }
+
+
+
+    }
+}
---
+++ src/Translator.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Translator {
+
+    Map<Integer,String> numericAlpha = new HashMap<>();
+    public Translator(Integer[] numeric, String[] alphabetic){
+
+        for (int i = 0; i < numeric.length; i++){
+            //Print possible variants:
+            // System.out.println(i);
+            numericAlpha.put( numeric[i], alphabetic[i] );
+
+        }
+
+    }
+
+    public String translate(Integer number){
+        return numericAlpha.get(number);
+    }
+
+}
+
